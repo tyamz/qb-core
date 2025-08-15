7f6311e04df7bc79e29bfa5b8574e256901a63df
Thomas Yamakaitis <tyamakaitis@gmail.com>
2025-08-15 15:50:46 -0400

    feat: emit player-paid event

--- config.lua
+++ config.lua
@@ -11,6 +11,7 @@ QBConfig.Money.DontAllowMinus = { 'cash', 'crypto' }                -- Money tha
 QBConfig.Money.MinusLimit = -5000                                    -- The maximum amount you can be negative 
 QBConfig.Money.PayCheckTimeOut = 10                                 -- The time in minutes that it will give the paycheck
 QBConfig.Money.PayCheckSociety = false                              -- If true paycheck will come from the society account that the player is employed at, requires qb-management
+QBConfig.Money.PayCheckInterval = true                              -- Toggle paycheck interval, set false to disable
 
 QBConfig.Player = {}
 QBConfig.Player.HungerRate = 4.2 -- Rate at which hunger goes down.
